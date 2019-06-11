71afacfaa922b6de085a07328befff634f74aac4
Alegatorr <aleksandr.a1.y@tumo.org>
2019-06-11 20:23:28 +0400

    Avelacum 1

---
+++ modules/LiveForm.js
@@ -0,0 +1,39 @@
+module.exports = class LiveForm {
+    constructor(x, y,life) {
+        this.life=life;
+        this.x = x;
+        this.y = y;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    die(arr){
+        matrix[this.y][this.x] = 0;
+
+        for (let i in arr) {
+            if (arr[i].x == this.x && arr[i].y == this.y) {
+                arr.splice(i, 1)
+            }
+        }
+    }
+    chooseCell(t) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == t) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+}
